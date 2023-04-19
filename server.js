bc0e123dd39354e0e67d11119f2b01ae46a9142f
mithun thambawita <mithunthambavita@gmail.com>
2023-04-19 21:04:44 +0530

    add public and text directory

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const path = require("path");
 //create the port
 const PORT = process.env.PORT || 3500;
 
+// build-in middleware to handle urlencoded data
+//in other words, form data
+// 'content-type application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+//build in middleware for json
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("^/$|/index(.html)?", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
   res.sendFile(path.join(__dirname, "views", "index.html"));
