6f19ac2d4f9674d9c38a53b36327175a48a4f4be
mithun thambawita <mithunthambavita@gmail.com>
2023-04-19 15:27:18 +0530

    example of get req

--- server.js
+++ server.js
@@ -17,6 +17,34 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "new-page.html");
 });
 
+//Route handlers
+app.get(
+  "/hello(,html)?",
+  (req, res, next) => {
+    console.log("attempt to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello world!");
+  }
+);
+
+//chaining route handlers
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res) => {
+  console.log("three");
+  res.send("Finished!");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
