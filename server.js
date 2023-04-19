1e712c77f87e3f03946716749a9ca399924a8ce8
mithun thambawita <mithunthambavita@gmail.com>
2023-04-19 12:11:15 +0530

    add some get req

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const path = require("path");
 //create the port
 const PORT = process.env.PORT || 3500;
 
-app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+app.get("^/$|/index(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "new-page.html");
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => {
